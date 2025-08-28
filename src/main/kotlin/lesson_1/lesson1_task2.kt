6b6e3d7b90560e86c7525a2e3a8731d526f52b67
Alexander <a_36524@mail.ru>
2025-08-28 05:45:56 +0400

    KS-1-2 other variables and print

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val numberOfOrders: Int = 75
+    val thanksMessage: String = "Спасибо за покупку в нашем магазине!"
+
+    println(numberOfOrders)
+    println(thanksMessage)
+
+    var numberOfEmployees: Int = 2000
+
+//    println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+
+    println(numberOfEmployees)
+}
