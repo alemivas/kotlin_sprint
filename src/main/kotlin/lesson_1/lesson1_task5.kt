88c54a5d9df765600de06948d638249a9cde27e0
Alexander <a_36524@mail.ru>
2025-08-30 08:04:25 +0400

    KS-1-5 solved

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,25 +1,22 @@
 package org.example.lesson_1
 
+const val SECONDS_IN_MINUTE = 60
+const val MINUTES_IN_HOUR = 60
+const val SECONDS_IN_HOUR = SECONDS_IN_MINUTE * MINUTES_IN_HOUR
+
 fun main() {
 
     val fullSeconds: Short = 6480
 
     // остаток секунд без целых часов
-    val secondsWoHours: Short = (fullSeconds % 3600).toShort()
+    val secondsWoHours: Short = (fullSeconds % SECONDS_IN_HOUR).toShort()
 
-    var hours: String = (fullSeconds/3600).toString()
+    val hours: Byte = (fullSeconds / SECONDS_IN_HOUR).toByte()
 
-    var seconds: String = (secondsWoHours % 60).toString()
+    val seconds: Byte = (secondsWoHours % SECONDS_IN_MINUTE).toByte()
 
-    var minutes: String = (secondsWoHours/60).toString()
+    val minutes: Byte = (secondsWoHours / SECONDS_IN_MINUTE).toByte()
 
-    if (hours.length == 1)
-        hours = "0$hours"
-    if (minutes.length == 1)
-        minutes = "0$minutes"
-    if (seconds.length == 1)
-        seconds = "0$seconds"
-
-    println("$hours:$minutes:$seconds")
+    println("%02d:%02d:%02d".format(hours, minutes, seconds))
 
 }
