4fa0ad6d771d8a41231c8dce2c4c1e00280a0c64
Alexander <a_36524@mail.ru>
2025-08-30 19:19:30 +0400

    KS-2-1 fix

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -2,13 +2,15 @@ package org.example.lesson_2
 
 fun main() {
 
-    val studentMark1: Byte = 3
-    val studentMark2: Byte = 4
-    val studentMark3: Byte = 3
-    val studentMark4: Byte = 5
+    val studentMark1 = 3
+    val studentMark2 = 4
+    val studentMark3 = 3
+    val studentMark4 = 5
 
-    val averageMark: Float = (studentMark1 + studentMark2
-            + studentMark3 + studentMark4) / 4f
+    val numbersOfStudent = 4
+
+    val averageMark: Float = (studentMark1 + studentMark2 +
+            studentMark3 + studentMark4).toFloat() / numbersOfStudent
 
     println("%.2f".format(averageMark))
 
