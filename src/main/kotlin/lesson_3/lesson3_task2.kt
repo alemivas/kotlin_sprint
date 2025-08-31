2868c7675acd6b0246fbecccea4483ef7fb50fd4
Alexander <a_36524@mail.ru>
2025-08-31 11:06:58 +0400

    KS-3-2 surname

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val name = "Татьяна"
+    var lastname = "Андреева"
+    val patronymic = "Сергеевна"
+    var age = 20
+
+    println("$lastname $name $patronymic, $age лет(года)")
+
+    lastname = "Сидорова"
+    age = 22
+
+    println("$lastname $name $patronymic, $age лет(года)")
+
+}
