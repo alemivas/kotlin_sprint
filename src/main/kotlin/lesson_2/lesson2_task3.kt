981a3127c6a039ae0cfb62b83c5eb2a09fec8a85
Alexander <a_36524@mail.ru>
2025-08-31 07:10:31 +0400

    KS-2-3 revision

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,18 +1,21 @@
 package org.example.lesson_2
 
-const val MINUTES_IN_HOUR: Byte = 60
+const val MINUTES_IN_HOUR = 60
+const val HOURS_IN_DAY = 24
 
 fun main() {
 
+    val departureHours = 9
+    val departureMinutes = 39
+    val travelTimeInMinutes = 457
 
-    val departureHours: Byte = 9
-    val departureMinutes: Byte = 39
-    val travelTimeInMinutes: Short = 457
+    val departureTimeInMinutes =
+        (departureHours * MINUTES_IN_HOUR + departureMinutes)
+    val arrivalTimeInMinutes = (departureTimeInMinutes + travelTimeInMinutes)
 
-    val departureTimeInMinutes: Short =
-        (departureHours * MINUTES_IN_HOUR + departureMinutes).toShort()
-    val arrivalTimeInMinutes: Short = (departureTimeInMinutes + travelTimeInMinutes).toShort()
+    // время прибытия с учетом смены суток
+    val arrivalHours = arrivalTimeInMinutes / MINUTES_IN_HOUR % HOURS_IN_DAY
+    val arrivalMinutes = arrivalTimeInMinutes % MINUTES_IN_HOUR
 
-
-    //учесть переход суток
+    println("Время прибытия поезда: %02d:%02d".format(arrivalHours, arrivalMinutes))
 }
