b72a3e507a6374d345c16915d9f940af1fc8d2d3
Alexander <a_36524@mail.ru>
2025-09-01 21:03:52 +0400

    KS-5-2 age verification

---
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_5
+
+fun main() {
+
+    val currentYear = 2025
+
+    val yearOfBirth = readln().toInt()
+
+    if ((currentYear - yearOfBirth) >= AGE_OF_MAJORITY)
+        println("Показать экран со скрытым контентом")
+    else
+        println("Доступ к скрытому контенту закрыт")
+
+}
+
+const val AGE_OF_MAJORITY = 18
