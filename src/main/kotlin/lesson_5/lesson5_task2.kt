177d938ec0e5d120f7671d9fb678b20f1e576ec5
Alexander <a_36524@mail.ru>
2025-09-02 05:49:40 +0400

    KS-5-2 age verification

--- src/main/kotlin/lesson_5/lesson5_task2.kt
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -4,6 +4,7 @@ fun main() {
 
     val currentYear = 2025
 
+    println("Введите год вашего рождения")
     val yearOfBirth = readln().toInt()
 
     if ((currentYear - yearOfBirth) >= AGE_OF_MAJORITY)
