850a18b56fdb5fff5be2a98d7c36f4aea3e116b7
Alexander <a_36524@mail.ru>
2025-09-02 05:00:46 +0400

    KS-4-4 fix

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -4,16 +4,14 @@ fun main() {
 
     val trainingDay = 5
 
-    val isExercisesForHands = (trainingDay % 2) == 1
-    val isExercisesForPress = (trainingDay % 2) == 1
-    val isExercisesForLegs = (trainingDay % 2) == 0
-    val isExercisesForBack = (trainingDay % 2) == 0
+    val isExercisesForHandsAndPress = (trainingDay % 2) == 1
+    val isExercisesForLegsAndBack = (trainingDay % 2) == 0
 
     println("""
-        Упражнения для рук:    $isExercisesForHands
-        Упражнения для ног:    $isExercisesForLegs
-        Упражнения для спины:  $isExercisesForBack
-        Упражнения для пресса: $isExercisesForPress
+        Упражнения для рук:    $isExercisesForHandsAndPress
+        Упражнения для ног:    $isExercisesForLegsAndBack
+        Упражнения для спины:  $isExercisesForLegsAndBack
+        Упражнения для пресса: $isExercisesForHandsAndPress
     """.trimIndent())
 
 }
