7c3714ee4aa2fcfadf7670eaec05ca7505360c98
Alexander <a_36524@mail.ru>
2025-09-02 04:47:43 +0400

    KS-4-3 fix

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,16 +1,22 @@
 package org.example.lesson_4
 
-const val WINTER = "winter"
-
 fun main() {
 
-    val isSunnyWeatherToday = true
-    val isAwningOpen = true
-    val airHumidity = 20
+    val isSunnyWeatherToday = FAVORABLE_SUNNY_WEATHER
+    val isAwningOpen = FAVORABLE_AWNING_OPEN
+    val airHumidity = FAVORABLE_AIR_HUMIDITY
     val season = WINTER
 
-    val isFavorableConditions = isSunnyWeatherToday && isAwningOpen &&
-            airHumidity == 20 && season != WINTER
+    val isFavorableConditions =
+            isSunnyWeatherToday == FAVORABLE_SUNNY_WEATHER &&
+            isAwningOpen == FAVORABLE_AWNING_OPEN &&
+            airHumidity == FAVORABLE_AIR_HUMIDITY &&
+            season != WINTER
 
     println("Благоприятные ли условия сейчас для роста бобовых? $isFavorableConditions")
 }
+
+const val FAVORABLE_SUNNY_WEATHER = true
+const val FAVORABLE_AWNING_OPEN = true
+const val FAVORABLE_AIR_HUMIDITY = 20
+const val WINTER = "winter"
