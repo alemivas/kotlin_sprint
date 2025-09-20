bb03fb88d03bb581fd5d08c71615343ef6d20588
Alexander <a_36524@mail.ru>
2025-09-20 05:28:27 +0400

    KS-7-1 fix

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -3,24 +3,21 @@ package org.example.lesson_7
 fun main() {
 
     var password = ""
+    val charRange = MIN_CHAR..MAX_CHAR
+    val numberRange = MIN_NUMBER..MAX_NUMBER
+    var randomChar: Char
+    var randomNumber: Int
 
-    for (i in 1..NUMBER_OF_ITERATIONS){
-
-        val randomChar = (MIN_CHAR..MAX_CHAR).random()
-        val randomNumber = (MIN_NUMBER..MAX_NUMBER).random()
-
-        password += randomChar.toString() + randomNumber
-
+    for (i in 1..NUMBER_OF_ITERATIONS) {
+        randomChar = charRange.random()
+        randomNumber = numberRange.random()
+        password += "$randomChar$randomNumber"
     }
-
     println("Сгенерированный пароль: $password")
-
 }
 
 const val MIN_NUMBER = 0
 const val MAX_NUMBER = 9
-
 const val MIN_CHAR = 'a'
 const val MAX_CHAR = 'z'
-
 const val NUMBER_OF_ITERATIONS = 3
