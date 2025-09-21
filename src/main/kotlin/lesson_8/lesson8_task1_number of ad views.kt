d17404502d7498a16615ac6485014bc846d77517
Alexander <a_36524@mail.ru>
2025-09-21 18:54:30 +0400

    KS-8-1-number-of-ad-views.kt

---
+++ src/main/kotlin/lesson_8/lesson8_task1_number of ad views.kt
@@ -0,0 +1,8 @@
+package org.example.lesson_8
+
+fun main() {
+    println("=== Количество просмотров рекламы ===")
+    val weekAdViews = arrayOf(50, 100, 120, 30, 150, 210, 212)
+    val weekAdViewsSum = weekAdViews.sum()
+    println("\nКоличество просмотров рекламы за неделю: $weekAdViewsSum")
+}
