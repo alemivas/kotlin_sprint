78820c7de84f4fc810ca039264a74e212aa75841
Alexander <a_36524@mail.ru>
2025-10-07 05:10:41 +0400

    KS-9-3-fix

--- src/main/kotlin/lesson_9/lesson9_task3 recipe-(ingredients amount).kt
+++ src/main/kotlin/lesson_9/lesson9_task3 recipe-(ingredients amount).kt
@@ -3,21 +3,19 @@ package org.example.lesson_9
 fun main() {
     println("=== Рассчет количества ингредиентов ===")
     println()
-    val ingredientsAmount = mutableListOf(2, 50, 15)
+    val ingredientsAmountForOneServing = listOf(2, 50, 15)
     println("Для одной порции нужно: " +
-            "Яиц – ${ingredientsAmount[0]} шт., " +
-            "молока – ${ingredientsAmount[1]} мл, " +
-            "сливочного масла – ${ingredientsAmount[2]} г"
+            "Яиц – ${ingredientsAmountForOneServing[0]} шт., " +
+            "молока – ${ingredientsAmountForOneServing[1]} мл, " +
+            "сливочного масла – ${ingredientsAmountForOneServing[2]} г"
     )
     println()
     println("Сколько порций хотите приготовить?")
     val servingsAmount = readln().toInt()
-    ingredientsAmount.forEachIndexed { index, value ->
-        ingredientsAmount[index] = value * servingsAmount
-    }
+    val ingredientsAmountForServingsAmount = ingredientsAmountForOneServing.map { it * servingsAmount }
     println("На $servingsAmount порций вам понадобится: " +
-            "Яиц – ${ingredientsAmount[0]} шт., " +
-            "молока – ${ingredientsAmount[1]} мл, " +
-            "сливочного масла – ${ingredientsAmount[2]} г"
+            "Яиц – ${ingredientsAmountForServingsAmount[0]} шт., " +
+            "молока – ${ingredientsAmountForServingsAmount[1]} мл, " +
+            "сливочного масла – ${ingredientsAmountForServingsAmount[2]} г"
     )
 }
