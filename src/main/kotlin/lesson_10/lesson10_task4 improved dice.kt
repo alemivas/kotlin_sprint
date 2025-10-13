d88329fa5511536daacbb649e6d30a37ca689faa
Alexander <a_36524@mail.ru>
2025-10-13 06:06:32 +0400

    KS-10-4 fix

--- src/main/kotlin/lesson_10/lesson10_task4 improved dice.kt
+++ src/main/kotlin/lesson_10/lesson10_task4 improved dice.kt
@@ -5,7 +5,7 @@ fun main() {
     var wonRoundsAmount = 0
 
     do {
-        if (playRound() == USER_WON) wonRoundsAmount++
+        if (playRound()) wonRoundsAmount++
         println("Хотите бросить кости еще раз? Введите $YES_USER_ANSWER или $NO_USER_ANSWER")
     } while (readln().equals(YES_USER_ANSWER, ignoreCase = true))
 
@@ -13,28 +13,38 @@ fun main() {
     println("Количество побед пользователя: $wonRoundsAmount")
 }
 
-fun playRound(): String {
+fun playRound(): Boolean {
     println()
     val userDiceNumber = rollGameDice()
     println("Ход игрока: выпало $userDiceNumber")
     val aiDiceNumber = rollGameDice()
     println("Ход компьютера: выпало $aiDiceNumber")
-    println()
 
     return when {
-        userDiceNumber > aiDiceNumber -> USER_WON
-        userDiceNumber < aiDiceNumber -> AI_WON
-        else -> FRIENDSHIP_WON
+        userDiceNumber > aiDiceNumber -> {
+            println("Победил пользователь")
+            println()
+            true
+        }
+
+        userDiceNumber < aiDiceNumber -> {
+            println("Победила машина")
+            println()
+            false
+        }
+
+        else -> {
+            println("Победила дружба")
+            println()
+            false
+        }
     }
 }
 
-fun rollGameDice() =            //переименовал функцию для исключения конфликта
-    (MINIMAL_DICE_NUMBER..MAXIMAL_DICE_NUMBER).random()
+fun rollGameDice() = (MINIMAL_DICE_NUMBER..MAXIMAL_DICE_NUMBER).random()
 
 const val MINIMAL_DICE_NUMBER = 1
 const val MAXIMAL_DICE_NUMBER = 6
-const val USER_WON = "Победил пользователь"
-const val AI_WON = "Победила машина"
-const val FRIENDSHIP_WON = "Победила дружба"
+
 const val YES_USER_ANSWER = "Да"
 const val NO_USER_ANSWER = "Нет"
