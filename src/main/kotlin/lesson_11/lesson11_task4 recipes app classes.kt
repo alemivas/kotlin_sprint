56ec5e87551e5f0041feca7fcbb074820d62de14
Alexander <a_36524@mail.ru>
2025-10-31 06:14:15 +0400

    KS-11-4 recipes-app-classes

---
+++ src/main/kotlin/lesson_11/lesson11_task4 recipes app classes.kt
@@ -0,0 +1,46 @@
+package org.example.lesson_11
+
+fun main() {
+    println("=== Классы для приложения рецептов ===")
+}
+
+class Category(
+    val id: Int,
+    val name: String,
+    val image: String,
+    val description: String,
+    val recipes: List<Recipe>,
+) {}
+
+class Recipe(
+    val id: Int,
+    val name: String,
+    val image: String,
+    var inFavorites: Boolean = false,
+    val category: Category,
+    val ingredients: List<Ingredient>,
+    val cookingMethod: String,
+) {
+    fun addToFavorites() {
+        inFavorites = true
+        println("Рецепт $name добавлен в избранное")
+    }
+
+    fun removeFromFavorites() {
+        inFavorites = false
+        println("Рецепт $name удалён из избранного")
+    }
+
+    fun getIngredients(servings: Int) =
+        ingredients.forEach { ingredient ->
+            "${ingredient.name}\t${ingredient.amount * servings} ${ingredient.units}\n"
+        }
+}
+
+class Ingredient(
+    val id: Int,
+    val name: String,
+    val amount: Int,
+    val units: String,
+    val recipe: Recipe,
+) {}
