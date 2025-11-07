85be1bbbab17694635d2c40682418eea9d6f4781
Alexander <a_36524@mail.ru>
2025-11-07 05:33:11 +0400

    KS-11-5 forum (wo Builder)

--- src/main/kotlin/lesson_11/lesson11_task5 forum.kt
+++ src/main/kotlin/lesson_11/lesson11_task5 forum.kt
@@ -8,38 +8,45 @@ fun main() {
     forum.createNewUser(name = "Вася")
     forum.createNewUser(name = "Петя")
 
-    forum.createNewMessage(userId = 1)
-    forum.createNewMessage(userId = 1)
+    forum.createNewMessage(userId = 0, message = "Доброе утро!")
+    forum.createNewMessage(userId = 1, message = "Привет!")
+    forum.createNewMessage(userId = 0, message = "Как дела?")
+    forum.createNewMessage(userId = 1, message = "Хорошо")
 
     forum.printThread()
 }
 
 class Forum(
     val userList: MutableList<ForumUser> = mutableListOf(),
+    val messageList: MutableList<ForumMessage> = mutableListOf(),
 ) {
+    var userId = 0
+
     fun createNewUser(name: String): ForumUser {
         val user = ForumUser(
-            userId = 1,
+            userId = userId++,
             userName = name,
         )
         userList.add(user)
         return user
     }
 
-    fun createNewMessage(userId: Int) {
+    fun createNewMessage(userId: Int, message: String) {
         if (userList.firstOrNull { it.userId == userId } != null) {
-            val message = ForumMessage(
+            val forumMessage = ForumMessage(
                 authorId = userId,
-                message = ""
+                message = message,
             )
+            messageList.add(forumMessage)
         }
     }
 
     fun printThread() {
-        userList.forEach { user ->
-            user.messageList.forEach { message ->
-                println("${user.userName}: ${message.message}")
-            }
+        println()
+        messageList.forEach { forumMessage ->
+            val authorId = forumMessage.authorId
+            val user = userList.first { it.userId == authorId }
+            println("${user.userName}: ${forumMessage.message}")
         }
     }
 }
@@ -47,7 +54,6 @@ class Forum(
 class ForumUser(
     val userId: Int,
     val userName: String,
-    val messageList: MutableList<ForumMessage> = mutableListOf(),
 ) {}
 
 class ForumMessage(
