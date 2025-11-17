ff089df3c63a5802cdd22df371cacdf73621980d
Alexander <a_36524@mail.ru>
2025-11-17 06:33:59 +0400

    KS-12-1 weather

---
+++ src/main/kotlin/lesson_12/lesson12_task1 weather.kt
@@ -0,0 +1,43 @@
+package org.example.lesson_12
+
+fun main() {
+    println("=== Форум ===")
+
+    val weatherDay1 = Weather()
+    weatherDay1.dayTemperature = 20
+    weatherDay1.nightTemperature = 15
+    weatherDay1.precipitation = false
+    weatherDay1.printInfo()
+
+    val weatherDay2 = Weather()
+    weatherDay2.dayTemperature = 16
+    weatherDay2.nightTemperature = 13
+    weatherDay2.precipitation = true
+    weatherDay2.printInfo()
+
+    println("Изменённые данные о погоде:")
+
+    weatherDay1.dayTemperature = 10
+    weatherDay1.nightTemperature = 7
+    weatherDay1.precipitation = true
+    weatherDay1.printInfo()
+
+    weatherDay2.dayTemperature = 25
+    weatherDay2.nightTemperature = 20
+    weatherDay2.precipitation = false
+    weatherDay2.printInfo()
+}
+
+class Weather {
+    var dayTemperature = 0
+    var nightTemperature = 0
+    var precipitation = false
+
+    fun printInfo() {
+        println()
+        println("Погода на день:")
+        println("Дневная температура $dayTemperature °C")
+        println("Ночная температура $nightTemperature °C")
+        println("Наличие осадков $precipitation")
+    }
+}
