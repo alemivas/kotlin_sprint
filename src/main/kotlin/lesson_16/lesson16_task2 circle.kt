ab5e521a58e99385fcfa49d24711d621d2eab692
Alexander <a_36524@mail.ru>
2026-01-21 06:19:56 +0400

    KS-16-2 circle fix

--- src/main/kotlin/lesson_16/lesson16_task2 circle.kt
+++ src/main/kotlin/lesson_16/lesson16_task2 circle.kt
@@ -5,20 +5,18 @@ import kotlin.math.pow
 fun main() {
     println("=== Длина окружности и площадь круга ===")
 
-    val circle = Circle(radius = 10)
+    val circle = Circle(radius = 10.0)
     println()
-    val circumference = "%.2f".format(circle.calculateCircumference())
-    println("Длина окружности = $circumference")
-    val circleArea = "%.2f".format(circle.calculateArea())
-    println("Площадь круга = $circleArea")
+    println("Длина окружности = ${"%.2f".format(circle.getCircumference())}")
+    println("Площадь круга = ${"%.2f".format(circle.getArea())}")
 }
 
 class Circle(
-    private val radius: Int
+    private val radius: Double
 ) {
-    fun calculateCircumference() = 2 * PI * radius
+    fun getCircumference() = 2 * PI * radius
 
-    fun calculateArea() = PI * radius.toDouble().pow(2)
+    fun getArea() = PI * radius.pow(2)
 }
 
 private const val PI = 3.14
