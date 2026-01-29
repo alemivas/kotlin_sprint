a6c8a3e80c92064f263dad45a93031eb2a1f6b06
Alexander <a_36524@mail.ru>
2026-01-29 06:17:32 +0400

    KS-17-2 ship-class

--- src/main/kotlin/lesson_17_Getter_and_Setter/lesson17_task2 ship class.kt
+++ src/main/kotlin/lesson_17_Getter_and_Setter/lesson17_task2 ship class.kt
@@ -3,17 +3,22 @@ package org.example.lesson_17_Getter_and_Setter
 fun main() {
     println("=== Класс Корабль ===")
     println()
-    val ship = Ship()
-    ship.name = "fghfg"
-    println(ship.name)
+    val ship = Ship(name = "Быстрый", averageSpeed = 50, homePort = "Мурманск")
+    ship.name = "Скорый"
+    println("Имя корабля ${ship.name}")
 }
 
-class Ship {
-    var name: String = "имя по умолчанию"
+class Ship(
+    name: String,
+    private val averageSpeed: Int,
+    private val homePort: String,
+) {
+    init {
+        println("Создан корабль $name")
+    }
+
+    var name = name
         set(value) {
             println("Имя корабля менять нельзя")
         }
-
-    val averageSpeed: Int = 0
-    val homePort: String = ""
 }
