9b270499f1812a4e65a2dce349a877a2da9e70f2
Alexander <a_36524@mail.ru>
2026-02-02 04:34:36 +0400

    KS-17-3 folders-and-files Fix

--- src/main/kotlin/lesson_17_Getter_and_Setter/lesson17_task3 folders and files.kt
+++ src/main/kotlin/lesson_17_Getter_and_Setter/lesson17_task3 folders and files.kt
@@ -3,19 +3,24 @@ package org.example.lesson_17_Getter_and_Setter
 fun main() {
     println("=== Папки и файлы ===")
     println()
-    val hiddenFolder = Folder(name = "Скрытая папка", filesNumber = 5, isHidden = true)
-    println(hiddenFolder.name)
-    val regularFolder = Folder(name = "Обычная папка", filesNumber = 13, isHidden = false)
-    println(regularFolder.name)
+    val hiddenFolder = Folder(name = "Личное", filesNumber = 5, isHidden = true)
+    println("Имя папки: ${hiddenFolder.name}. Количество файлов: ${hiddenFolder.filesNumber}")
+    val regularFolder = Folder(name = "Общее", filesNumber = 13, isHidden = false)
+    println("Имя папки: ${regularFolder.name}. Количество файлов: ${regularFolder.filesNumber}")
 }
 
 class Folder(
     name: String,
-    private val filesNumber: Int,
+    filesNumber: Int,
     private val isHidden: Boolean,
 ) {
     val name = name
         get() =
-            if (isHidden) "Скрытая папка. Количество файлов – 0"
-            else "$field. Количество файлов – $filesNumber"
+            if (isHidden) "Скрытая папка"
+            else field
+
+    val filesNumber = filesNumber
+        get() =
+            if (isHidden) 0
+            else field
 }
