24708883f19eaadff9eef66a77f73feb5a13b63d
Alexander <a_36524@mail.ru>
2026-02-18 06:31:30 +0400

    KS-19-3 spacecraft

---
+++ src/main/kotlin/lesson_19_Enum_Todo_Nothing/lesson19_task3 spacecraft.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_19_Enum_Todo_Nothing
+
+fun main() {
+    println("=== Космический корабль ===")
+    println()
+}
+
+class Spacecraft {
+    fun takeOff() {
+        // TODO()
+    }
+    fun land() {}
+    fun shoot() {}
+}
