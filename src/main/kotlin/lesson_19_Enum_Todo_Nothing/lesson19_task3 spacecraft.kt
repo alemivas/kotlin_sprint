9b30eccfbcc732e2f0a674df498ad662cb03ab5b
Alexander <a_36524@mail.ru>
2026-02-20 04:53:40 +0400

    KS-19-3 spacecraft

--- src/main/kotlin/lesson_19_Enum_Todo_Nothing/lesson19_task3 spacecraft.kt
+++ src/main/kotlin/lesson_19_Enum_Todo_Nothing/lesson19_task3 spacecraft.kt
@@ -3,12 +3,20 @@ package org.example.lesson_19_Enum_Todo_Nothing
 fun main() {
     println("=== Космический корабль ===")
     println()
+    val spacecraft = Spacecraft()
+    spacecraft.takeOff()
+    spacecraft.land()
+    spacecraft.shoot()
 }
 
 class Spacecraft {
     fun takeOff() {
-        // TODO()
+        // TODO нужна информация о дополнительной логике
     }
+
     fun land() {}
-    fun shoot() {}
+
+    fun shoot() {
+        TODO("нужна реализация")
+    }
 }
