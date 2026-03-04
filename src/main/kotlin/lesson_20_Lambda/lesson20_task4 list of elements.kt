a8dfe7f3b857b7c469f7ef623b7d4ad4c0d45148
Alexander <a_36524@mail.ru>
2026-03-04 06:29:58 +0400

    KS-20-4 list-of-elements

--- src/main/kotlin/lesson_20_Lambda/lesson20_task4 list of elements.kt
+++ src/main/kotlin/lesson_20_Lambda/lesson20_task4 list of elements.kt
@@ -3,23 +3,8 @@ package org.example.lesson_20_Lambda
 fun main() {
     println("=== Список элементов ===")
     println()
-
     val categories = listOf("Фрукты", "Молочные", "Хлебные", "Овощи", "Мясные", "Кондитерские")
-
-    val lambda: (String) -> Unit = { println("Нажат элемент $it") }
-
-//    val clickAllCategories = categories.map { println("Нажат элемент $it") }
-    categories.map(lambda)
-//    val clickedCategories = categories.map { println("Нажат элемент $it") }.filterIndexed { index, _ -> index % 2 == 0 }
-//    val clickedCategories = categories.filterIndexed { index, _ -> index % 2 == 0 }.map { println("Нажат элемент $it") }
-//    val clickedCategories = categories.mapIndexed { index, string ->
-//        index % 2 == 0
-//        println("Нажат элемент $string")
-//    }
-//    val clickedCategories = categories.filterIndexed { index, string ->
-//        println("Нажат элемент $string")
-//        index % 2 == 0
-//    }
-
-//    val clickEvenCategories =
+    categories
+        .filterIndexed { index, _ -> index % 2 == 0 }
+        .map { println("Нажат элемент $it") }
 }
