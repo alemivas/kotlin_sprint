29e04442b969c9e05e186ff45937c628520f4576
Alexander <a_36524@mail.ru>
2026-03-06 05:44:23 +0400

    KS-20-4 list-of-elements FIX

--- src/main/kotlin/lesson_20_Lambda/lesson20_task4 list of elements.kt
+++ src/main/kotlin/lesson_20_Lambda/lesson20_task4 list of elements.kt
@@ -3,8 +3,11 @@ package org.example.lesson_20_Lambda
 fun main() {
     println("=== Список элементов ===")
     println()
-    val categories = listOf("Фрукты", "Молочные", "Хлебные", "Овощи", "Мясные", "Кондитерские")
-    categories
-        .filterIndexed { index, _ -> index % 2 == 0 }
-        .map { println("Нажат элемент $it") }
+    val categoriesList = listOf("Фрукты", "Молочные", "Хлебные", "Овощи", "Мясные", "Кондитерские")
+    val categoriesLambdaList: List<() -> Unit> = categoriesList.map { category ->
+        { println("Нажат элемент $category") }
+    }
+    categoriesLambdaList.forEachIndexed { index, lambda ->
+        if (index % 2 == 0) lambda()
+    }
 }
