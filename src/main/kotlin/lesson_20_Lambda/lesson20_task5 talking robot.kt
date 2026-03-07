7d76ed581577bab528ebed0ae623376ba15ff513
Alexander <a_36524@mail.ru>
2026-03-07 07:01:49 +0400

    KS-20-5 talking-robot

--- src/main/kotlin/lesson_20_Lambda/lesson20_task5 talking robot.kt
+++ src/main/kotlin/lesson_20_Lambda/lesson20_task5 talking robot.kt
@@ -4,21 +4,28 @@ fun main() {
     println("=== Говорящий робот ===")
     println()
     val robot = Robot()
-    val modifier: (String) -> String = { it.reversed() }
     robot.say()
+    val modifier: (String) -> String = { it.reversed() }
     robot.setModifier(modifier)
     robot.say()
 }
 
 class Robot {
     private val phraseList = listOf("Привет!", "Здравствуйте!", "Добрый день!", "Салют!", "Хеллоу!")
-    private var phrase = phraseList.random()
+    private var phraseModifier: (String) -> String = { it }
+
+    init {
+        println("(Создан говорящий робот без модификатора речи)")
+    }
 
     fun say() {
+        var phrase = phraseList.random()
+        phrase = phraseModifier(phrase)
         println(phrase)
     }
 
-    fun setModifier(modifier: (String) -> String) {
-        phrase = modifier(phrase)
+    fun setModifier(newModifier: (String) -> String) {
+        phraseModifier = newModifier
+        println("(Установлен модификатор речи)")
     }
 }
