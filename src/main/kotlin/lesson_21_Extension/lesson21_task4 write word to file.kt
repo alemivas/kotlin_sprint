514ad61c545269193b8f5abf090673e8951949f7
Alexander <a_36524@mail.ru>
2026-03-15 07:51:03 +0400

    KS-21-4 write-word-to-file

---
+++ src/main/kotlin/lesson_21_Extension/lesson21_task4 write word to file.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_21_Extension
+
+import java.io.File
+
+fun main() {
+    println("=== Запись слова в начало файла ===")
+    println()
+    val file = File("text.txt")
+    file.createNewFile()
+    file.prependText("dfGgsUIdgdg")
+    file.prependText("Ttttttt")
+    file.prependText("uUuUuUuU")
+}
+
+fun File.prependText(word: String) {
+    val lowercaseWord = word.lowercase()
+    val text = readText()
+    writeText(lowercaseWord + "\n" + text)
+    println("В начало файла $name записано \"$lowercaseWord\"")
+}
